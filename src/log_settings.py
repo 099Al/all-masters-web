0d34d38c916536d4e458a9bc7a2d4f1f42463e82
efimov_as <efimov_as@tochka.com>
2025-08-31 23:38:55 +0500

    logging

---
+++ src/log_settings.py
@@ -0,0 +1,38 @@
+import logging.config
+import os
+
+LOG_FILE = 'logs/shop.log'
+os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(levelname)s - %(asctime)s - %(name)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'level': 'ERROR',
+            'formatter': 'default',
+            'filename': LOG_FILE,
+            'mode': 'a',
+            'encoding': 'utf8',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'ERROR',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'root': {
+        'level': 'ERROR',
+        'handlers': ['file', 'console'],
+    },
+}
+
+
+logging.config.dictConfig(LOGGING_CONFIG)
