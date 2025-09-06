a43e8a5474e0c05df0efa8e7a5f443f6bcf9f8bd
al <walkmore@yandex.ru>
2025-09-06 18:09:45 +0500

    avatars to html

--- src/routers/profiles.py
+++ src/routers/profiles.py
@@ -1,15 +1,35 @@
+import os
+
+from urllib import request
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from pathlib import Path
 
 from fastapi.templating import Jinja2Templates
+from fastapi import HTTPException
+
+from starlette.responses import FileResponse
 
 from src.database.requests_web import ReqWeb
+import mimetypes
 
 router_profiles = APIRouter(
     prefix="/profiles",
     tags=["profile"],
 )
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # текущая папка файла роутера
+PROJECT_DIR = os.path.dirname(os.path.dirname(BASE_DIR))  # поднимаемся в Project
+AVATAR_DIR = os.path.join(PROJECT_DIR, "images", "avatars")
+
+@router_profiles.get("/avatar/{filename}", name="get_avatar")
+async def get_avatar(filename: str):
+    file_path = os.path.join(AVATAR_DIR, filename)
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Avatar not found")
+    mime_type, _ = mimetypes.guess_type(file_path)
+    return FileResponse(file_path, media_type=mime_type or "application/octet-stream")
+    #return FileResponse(file_path, media_type="image/jpeg")
 
 
 templates = Jinja2Templates(directory="src/templates")
