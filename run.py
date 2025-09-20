94843bb1ae45797d2132576bed7c352e034675c4
al <walkmore@yandex.ru>
2025-09-20 15:06:32 +0500

    sert  slider version1

--- run.py
+++ run.py
@@ -12,4 +12,6 @@ if __name__ == "__main__":
     uvicorn.run(app="src.main:app", reload=True, host='127.0.0.1', port=8000, workers=2)
     """
 
-    uvicorn.run(app="src.main:app", reload=True, host='127.0.0.1', port=8001, workers=1)
+    uvicorn.run(app="src.main:app", reload=True, host='127.0.0.1', port=8001, workers=1
+                 , ssl_certfile="/certs/cert.pem", ssl_keyfile="/certs/key.pem"
+               )
