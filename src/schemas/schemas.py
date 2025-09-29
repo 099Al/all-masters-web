f89e8922579cce09c553b2b0b44faab39cf6f043
al <walkmore@yandex.ru>
2025-09-29 11:01:05 +0500

    message sed message any times. focus after delete

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
-class UserMessageCreate(BaseModel):
+class MessageCreate(BaseModel):
     user_id: int        # от кого
     specialist_id: int  # кому
     message: str
+
+
+class MessageUpdate(BaseModel):
+    message: str
+
+class MessageOut(BaseModel):
+    id: int
+    user_id: int
+    specialist_id: int
+    message: str
+    created_at: datetime | None = None
